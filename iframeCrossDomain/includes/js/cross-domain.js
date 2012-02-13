246d205b7eb04d2de7c2c8e26ef4c7d7defcb090
Mohit Seth <mohit@sethmohit.com>
2012-02-13 14:08:18 -0600

    added domain to function

--- iframeCrossDomain/includes/js/cross-domain.js
+++ iframeCrossDomain/includes/js/cross-domain.js
@@ -7,20 +7,23 @@ Performs sub domain call using an iframe.
 Usage:
 	Create new object of CrossDomain object passing the initial parameters
 	E.g:
-	var cd = new CrossDomain($("#frameID"),$("#targetDivID"),"http://xyz.com",callbackFunction);
+	var cd = new CrossDomain($("#frameID"),$("#targetDivID"),"xyz.com","http://xyz.com",callbackFunction);
 	cd.getFrameContent();		//this function will perform all the operations required.
 	
 	
 	Important: document.domain has to be set to same on both sides.
 */
 
-var CrossDomain = function(frameObj,targetDiv,url,callback){
+var CrossDomain = function(frameObj,targetDiv,domain,url,callback){
 	this.frameObj = frameObj;
 	this.version ='1.1';
 	this.date = '02/10/2012';
 	this.targetDiv= targetDiv;
 	this.callback = callback;
 	this.url = url;
+	if(typeof domain !== 'undefined'){
+		document.domain = domain;
+	}
 };
 
 /* This Function gets the content for iframe and replaces to the target div element */
